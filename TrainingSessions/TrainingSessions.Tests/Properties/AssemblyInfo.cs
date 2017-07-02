da05cfd47c9ba64783255f6141d43e6c378dbecb
Petr Novák <novakp@vst.cz>
2017-07-02 14:18:43 +0200

    master

---
+++ TrainingSessions/TrainingSessions.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Obecné informace o sestavení jsou řízeny prostřednictvím následující
+// sady atributů. Chcete-li změnit informace související se sestavením,
+// změňte hodnoty těchto atributů.
+[assembly: AssemblyTitle("TrainingSessions.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TrainingSessions.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Nastavení atributu ComVisible na hodnotu false má za výsledek, že typy v tomto sestavení nejsou viditelné
+// pro komponenty modelu COM. Potřebujete-li přistupovat k typu v tomto sestavení
+// z modelu COM, nastavte atribut ComVisible pro daný typ na hodnotu true.
+[assembly: ComVisible(false)]
+
+// Následující GUID je určen pro ID knihovny typelib, pokud je tento projekt zpřístupněn modulu COM.
+[assembly: Guid("d5559e64-d966-43d0-8fb8-4b81e2ca9b03")]
+
+// Informace o verzi sestavení jsou tvořeny následujícími čtyřmi hodnotami:
+//
+//      Hlavní verze
+//      Podverze
+//      Číslo sestavení
+//      Revize
+//
+// Můžete zadat všechny tyto hodnoty, nebo použít výchozí hodnoty pro číslo revize a sestavení
+// zadáním znaku * jako v příkladu níže:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

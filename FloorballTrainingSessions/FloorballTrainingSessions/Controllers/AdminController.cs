6eb1320519beb37bae03e571f01538e17b3d392a
Petr Novák <novakp@vst.cz>
2017-07-14 23:32:07 +0200

    dd

--- FloorballTrainingSessions/FloorballTrainingSessions/Controllers/AdminController.cs
+++ FloorballTrainingSessions/FloorballTrainingSessions/Controllers/AdminController.cs
@@ -3,15 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
-namespace FloorballTrainingSessions.Controllers
+namespace FloorballTrainingSessions
 {
     public class AdminController : Controller
     {
-        // GET: Admin
-        public ActionResult Index()
+        private Entities db = new Entities();
+
+        public int GetPlayerID(string userid)
         {
-            return View();
+            var players = db.Players.Where(t => (t.User == userid)).FirstOrDefault();
+            return players.Id;
+        }
+        public string GetPlayerName(string userid)
+        {
+            var players = db.Players.Where(t => (t.User == userid)).FirstOrDefault();
+            return players.Name;
+        }
+        public int GetActiveSeason()
+        {
+            var seasons = db.Seasons.Where(t => (t.IsActiveSeason == true)).FirstOrDefault();
+            return seasons.Id;
+        }
+        public string GetSeasonName(int SeasonId)
+        {
+            var seasons = db.Seasons.Where(t => (t.Id == SeasonId)).FirstOrDefault();
+            return seasons.SeasonName;
+        }
+        // GET: Admin
+        public ActionResult Index(int? SeasonId)
+        {
+            AdminModel admins = new AdminModel();
+            admins.CurrentUser = User.Identity.GetUserId();
+            admins.CurrentPlayer = GetPlayerID(admins.CurrentUser);
+            admins.CurrentPlayerName = GetPlayerName(admins.CurrentUser);
+            admins.Seasons = db.Seasons.ToList();
+            admins.ActiveSeason = GetActiveSeason();
+            if (SeasonId != null) { admins.ActiveSeason = SeasonId.GetValueOrDefault(); };
+            admins.ActiveSeasonName = GetSeasonName(admins.ActiveSeason);
+            return View(admins);
         }
     }
 }
---
+++ FloorballTrainingSessions/FloorballTrainingSessions/Models/AdminModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace FloorballTrainingSessions
+{
+    public partial class AdminModel
+    {
+        public AdminModel()
+        {
+            this.Seasons = new HashSet<Seasons>();
+            this.Teamplayers = new HashSet<TeamPlayers>();
+
+        }
+        [NotMapped]
+        [Key]
+        public string CurrentUser { get; set; }
+        [NotMapped]
+        public int CurrentPlayer { get; set; }
+        public string CurrentPlayerName { get; set; }
+        public int ActiveSeason { get; set; }
+        public string ActiveSeasonName { get; set; }
+        public int SelectedSeason { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Seasons> Seasons { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<TeamPlayers> Teamplayers { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Trainings> Trainings { get; set; }
+    }
+}
+
+
+
